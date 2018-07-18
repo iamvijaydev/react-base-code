36c307f46a3c13ac0aad2a9ca5f0bcb3b80d3b19
Vijay Dev <vijaydev.in@gmail.com>
2018-07-18 13:03:31 +0530

    fixed ut errors

--- src/store/__tests__/locale.spec.js
+++ src/store/__tests__/locale.spec.js
@@ -2,25 +2,25 @@ import locale, { initalState } from '../data/locale';
 
 describe('locale reducer', () => {
   it('should return the initial state', () => {
-    expect(themes(undefined, {})).toBe(initalState);
+    expect(locale(undefined, {})).toBe(initalState);
   });
 
   it('should return with en-US selected', () => {
-    expect(themes(initalState, {
+    expect(locale(initalState, {
       type: 'CHANGE_LOCALE',
       id: 'en-US',
     }).selected.id).toBe('en-US');
   });
 
   it('should return with fr-FR selected', () => {
-    expect(themes(initalState, {
+    expect(locale(initalState, {
       type: 'CHANGE_LOCALE',
       id: 'fr-FR',
     }).selected.id).toBe('fr-FR');
   });
 
   it('should return with ja-JP selected', () => {
-    expect(themes(initalState, {
+    expect(locale(initalState, {
       type: 'CHANGE_LOCALE',
       id: 'ja-JP',
     }).selected.id).toBe('ja-JP');
